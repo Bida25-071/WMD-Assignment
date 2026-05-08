516fbf24b25bd625d7377bbb708a0e4e0d4d3c6c
Wame <Bida25-071@thuto.bac.ac.bw>
2026-05-08 17:13:11 +0200

    Update content

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-document.querySelector("form").addEventListener("submit",function(e) {
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.querySelector("form").addEventListener("submit",function(e) {
     e.preventDefault();
 
     document.getElementById("feedback").classList.remove("d-none");
+
+    setTimeout(function() {
+            feedback.classList.add("d-none");
+        }, 3000);
+
+  });
+    
 });
+
+const toggleButton = document.getElementById('theme-toggle'); 
+
+ 
+
+toggleButton.addEventListener('click', function() { 
+
+    document.body.classList.toggle('dark-mode'); 
+
+    if (document.body.classList.contains('dark-mode')) { 
+
+        toggleButton.textContent = 'Light Mode'; 
+
+    } else { 
+
+        toggleButton.textContent = 'Dark Mode'; 
+
+    } 
+
+}); 
